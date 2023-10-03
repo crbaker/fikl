ea3f391defa3514d8849ba12d3d6093ee3c65160
Chris Baker <chris@emguidance.com>
2023-10-03 22:12:40 +0200

    adding more repl features

--- lang/cli.py
+++ lang/cli.py
@@ -1,10 +1,14 @@
 """This module provides the fsql CLI."""
 # lang/cli.py
 
+import os.path
+import atexit
+
 import typer
-from typing_extensions import Annotated, Optional
 import json
 import readline
+from typing_extensions import Annotated, Optional
+from rich import print as rprint, print_json
 
 from lang import (__app_name__, __version__, ql)
 
@@ -24,23 +28,48 @@ def query(query_text: Annotated[Optional[str], typer.Argument(help="The FSQL que
 
 def run_query_and_output(query_text):
     results = ql.run_query(query_text)
-    typer.echo(json.dumps(results, indent=2))
+    print_json(json.dumps(results, indent=2))
 
 
 def start_repl():
     go_again = True
 
-    readline.parse_and_bind('tab: complete')
-    readline.parse_and_bind('set editing-mode vi')
+    history_path = os.path.expanduser("~/.fsql_history")
+
+    def save_history(history_path=history_path):
+        readline.write_history_file(history_path)
+
+    if os.path.exists(history_path):
+        readline.set_history_length(100)
+        readline.read_history_file(history_path)
+
+    atexit.register(save_history)
+
+    readline.parse_and_bind("tab: complete")
+    readline.parse_and_bind("set editing-mode vi")
+
+    rprint("[italic pink]FSQL Repl[/italic pink] :fire:")
+
+    current_query: str = None
 
     while go_again:
-        query_text = input('>: ')
 
-        if (query_text.strip() == "exit"):
-            go_again = False
-            typer.echo("Bye!")
+        if current_query is None:
+            current_query = input('> ').strip()
         else:
+            current_query = f"{current_query} {input(': ')}".strip()
+
+        if current_query == "exit":
+            go_again = False
+            rprint("Bye!:waving_hand:")
+        if current_query == "cls":
+            typer.clear()
+            current_query = None
+        elif current_query.endswith(';'):
             try:
-                run_query_and_output(query_text)
+                run_query_and_output(current_query[:-1])
             except Exception as e:
-                typer.echo(e)
+                rprint("[italic red]Query Error[/italic red] :exploding_head:")
+                rprint(e)
+            finally:
+                current_query = None
