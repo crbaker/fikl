8431dfe338e7765206ddb41d15091c48f8af81cd
Chris Baker <chris@emguidance.com>
2023-10-03 09:44:40 +0200

    update and delete supported

--- lang/ql.py
+++ lang/ql.py
@@ -1,24 +1,23 @@
 """This module provides the fsql query details."""
 # lang/ql.py
 
-# XWMHTQUM YBKOGUGP
 import os
 import sys
-from lark import Lark, Transformer, v_args, Tree
+from lark import Lark
 
 from rich import print as rprint
 
 from google.cloud import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 
-from lang.transformer import FSQLQuery, FSQLSubjectType, FSQLTree
+from lang.transformer import FSQLQuery, FSQLQueryType, FSQLShowQuery, FSQLSubjectType, FSQLTree, FSQLUpdateQuery
 
 
 class QueryError(ValueError):
     pass
 
 
-def run_query(query: str) -> list[dict]:
+def run_query(query: str) -> list[dict] | dict:
 
     grammar = read_grammar()
 
@@ -28,8 +27,19 @@ def run_query(query: str) -> list[dict]:
     ll = FSQLTree().transform(parse_tree)
     fsql_query: FSQLQuery = ll.children[0]
 
-    documents = execute_query(fsql_query)
-    return list(filter(object_exists, map(snapshot_to_document_fn(fsql_query), documents)))
+    response = execute_query(fsql_query)
+
+    if isinstance(response, int):
+        return {"count": response}
+    else:
+        return list(filter(object_exists, map(snapshot_to_document_fn(fsql_query), response)))
+
+
+def merge_dicts(dicts: list[dict]) -> dict:
+    result = {}
+    for d in dicts:
+        result.update({d["property"]: d["value"]})
+    return result
 
 
 def extract_fields(obj: dict, fields: list[str]):
@@ -59,7 +69,7 @@ def object_exists(dict) -> bool:
 
 
 def snapshot_to_document_fn(fsql_query: FSQLQuery):
-    requested_fields = fsql_query["fields"]
+    requested_fields = fsql_query["fields"] if "fields" in fsql_query else "*"
 
     def extract_fields_from_snapshot(snapshot: firestore.DocumentSnapshot):
         document_dict = snapshot.to_dict()
@@ -75,13 +85,68 @@ def snapshot_to_document_fn(fsql_query: FSQLQuery):
 
 def add_where(query: firestore.Query, fsql_query: FSQLQuery):
     if fsql_query["where"] is not None:
-        field_filter = FieldFilter(fsql_query["where"]["field"], fsql_query["where"]["operator"], fsql_query["where"]["value"])
+        field_filter = FieldFilter(
+            fsql_query["where"]["field"], fsql_query["where"]["operator"], fsql_query["where"]["value"])
         return query.where(filter=field_filter)
     else:
         return query
 
 
-def execute_query(fsql_query: FSQLQuery) -> list[firestore.DocumentSnapshot]:
+def update_document_ref(ref: firestore.DocumentReference, new_values: dict):
+    ref.update(new_values)
+
+def delete_document_ref(ref: firestore.DocumentReference):
+    ref.delete()
+
+def execute_query(fsql_query: FSQLQuery) -> list[firestore.DocumentSnapshot] | int:
+
+    def fn_for_query(fsql_query: FSQLQuery):
+        match fsql_query["query_type"]:
+            case FSQLQueryType.SHOW:
+                return execute_show_query
+            case FSQLQueryType.UPDATE:
+                return execute_update_query
+            case FSQLQueryType.DELETE:
+                return execute_delete_query
+            case _:
+                return lambda x: []
+
+    query_fn = fn_for_query(fsql_query)
+
+    return query_fn(fsql_query)
+
+def execute_delete_query(fsql_query: FSQLUpdateQuery) -> int:
+    docs = execute_show_query(fsql_query)
+
+    count = 0
+
+    for doc in docs:
+        try:
+            delete_document_ref(doc.reference)
+            count += 1
+        except Exception:
+            pass
+
+    return count
+
+
+def execute_update_query(fsql_query: FSQLUpdateQuery) -> int:
+    docs = execute_show_query(fsql_query)
+
+    count = 0
+
+    for doc in docs:
+        new_values = merge_dicts(fsql_query["set"])
+        try:
+            update_document_ref(doc.reference, new_values)
+            count += 1
+        except Exception:
+            pass
+
+    return count
+
+
+def execute_show_query(fsql_query: FSQLShowQuery) -> list[firestore.DocumentSnapshot]:
     db = firestore.Client()
 
     match fsql_query["subject_type"]:
@@ -98,13 +163,12 @@ def execute_query(fsql_query: FSQLQuery) -> list[firestore.DocumentSnapshot]:
 
 def resource_path(relative_path):
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS        
+        base_path = sys._MEIPASS
     except Exception:
-        base_path = os.environ.get("_MEIPASS2",os.path.abspath("."))
+        base_path = os.environ.get("_MEIPASS2", os.path.abspath("."))
 
-    os.listdir(base_path)
     return os.path.join(base_path, relative_path)
+
 
 def read_grammar():
     return open(resource_path("fsql.lark")).read()
--- lang/transformer.py
+++ lang/transformer.py
@@ -1,34 +1,50 @@
 from lark import Transformer, v_args, Tree
-from typing import List
 import ast
 from rich import print as rprint
 from enum import Enum
 from typing import TypedDict
 from typing import Union
 
-AllTypes = Union[int, float, str, bool]
+AllTypes = Union[int, float, str, bool, None]
+
 
 class FSQLQueryType(Enum):
     SHOW = 1
     UPDATE = 2
     DELETE = 3
 
+
 class FSQLSubjectType(Enum):
     COLLECTION_GROUP = 1
     DOCUMENT = 2
     COLLECTION = 3
 
+
 class FSQLWhere(TypedDict):
     field: str
     operator: str
-    value: int | float | str | List[any]
+    value: int | float | str | list[any]
+
 
 class FSQLQuery(TypedDict):
     query_type: FSQLQueryType
-    fields: list[str] | str
     subject: str
     subject_type: FSQLSubjectType
     where: FSQLWhere | None
+
+
+class FSQLUpdateSet(TypedDict):
+    property: str
+    value: AllTypes
+
+
+class FSQLUpdateQuery(FSQLQuery):
+    set: list[FSQLUpdateSet]
+
+
+class FSQLShowQuery(FSQLQuery):
+    fields: list[str] | str
+
 
 @v_args(inline=True)    # Affects the signatures of the methods
 class FSQLTree(Transformer):
@@ -36,16 +52,17 @@ class FSQLTree(Transformer):
         self.vars = {}
 
     def data_value(self, some_tree: Tree, data: str):
-        data_tree: List[Tree] = list(some_tree.find_data(data))
+        data_tree: list[Tree] = list(some_tree.find_data(data))
         return data_tree[0].children[0].value
-    
-    def as_fsql_match(self, where: Tree) -> AllTypes | List[AllTypes]:
+
+    def as_fsql_match(self, where: Tree) -> AllTypes | list[AllTypes]:
         matching = list(where.find_data('matching'))[0].children[0]
 
         if matching.data == 'literal':
             return ast.literal_eval(self.data_value(matching, "literal"))
         elif matching.data == "array":
-            values = list(map(lambda x: ast.literal_eval(x.children[0].value), list(matching.find_data("literal"))))
+            values = list(map(lambda x: ast.literal_eval(
+                x.children[0].value), list(matching.find_data("literal"))))
             return values
 
     def as_where(self, where: Tree | None) -> FSQLWhere | None:
@@ -61,36 +78,80 @@ class FSQLTree(Transformer):
                 'operator': operator,
                 'value': matching
             }
-        
+
     def as_fields(self, select: Tree) -> list[str]:
         select_type = select.children[0].data.value
         if select_type == "fields":
-            values = list(map(lambda x: x.children[0].value if x.children[0].type == "CNAME" else ast.literal_eval(x.children[0].value), list(select.children[0].find_data("property"))))
+            values = list(map(lambda x: self.as_value(x), list(
+                select.children[0].find_data("property"))))
             return values
         elif select_type == "all":
             return "*"
-        
-    def show(self, select: Tree, subject_type: Tree, subject: Tree, where: Tree | None):
 
-        def as_fsql_subject_type(subject_type: Tree):
-            match subject_type.children[0].value:
-                case "within":
-                    return FSQLSubjectType.COLLECTION_GROUP
-                case "from":
-                    return FSQLSubjectType.COLLECTION
-                case "at":
-                    return FSQLSubjectType.DOCUMENT
+    def as_value(self, some_tree: Tree) -> AllTypes:
+        match some_tree.children[0].type:
+            case "CNAME":
+                return some_tree.children[0].value
+            case "NULL":
+                return None
+            case _:
+                return ast.literal_eval(some_tree.children[0].value)
+
+    def as_fsql_subject_type(self, subject_type: Tree):
+        match subject_type.children[0].type:
+            case "WITHIN":
+                return FSQLSubjectType.COLLECTION_GROUP
+            case "FROM":
+                return FSQLSubjectType.COLLECTION
+            case "AT":
+                return FSQLSubjectType.DOCUMENT
+
+    def show(self, select: Tree, subject_type: Tree, subject: Tree, where: Tree | None) -> FSQLShowQuery:
 
         return {
             "query_type": FSQLQueryType.SHOW,
-            "fields": self.as_fields(select) ,
-            "subject": subject.children[0].value if subject.children[0].type == "CNAME" else ast.literal_eval(subject.children[0].value),
-            "subject_type": as_fsql_subject_type(subject_type),
+            "fields": self.as_fields(select),
+            "subject": self.as_value(subject),
+            "subject_type": self.as_fsql_subject_type(subject_type),
             "where": self.as_where(where)
         }
 
-    def show_collection (self, select: Tree, subject_type: Tree, subject: Tree, where: Tree | None):
+    def show_collection(self, select: Tree, subject_type: Tree, subject: Tree, where: Tree | None):
         return self.show(select, subject_type, subject, where)
 
-    def show_document (self, select: Tree, subject_type: Tree, subject: Tree):
+    def show_document(self, select: Tree, subject_type: Tree, subject: Tree):
         return self.show(select, subject_type, subject, where=None)
+
+    def update(self, subject_type: Tree, subject: Tree, set: Tree, where: Tree | None):
+        setters = list(map(lambda x: {
+            "property": self.data_value(x, "property"),
+            "value": self.as_value(x.children[1])
+        }, list(set.find_data("setter"))))
+
+        return {
+            "query_type": FSQLQueryType.UPDATE,
+            "subject": self.as_value(subject),
+            "subject_type": self.as_fsql_subject_type(subject_type),
+            "where": self.as_where(where),
+            "set": setters
+        }
+
+    def update_collection(self, subject_type: Tree, subject: Tree, set: Tree, where: Tree):
+        return self.update(subject_type, subject, set, where)
+
+    def update_document(self, subject_type: Tree, subject: Tree, set: Tree):
+        return self.update(subject_type, subject, set, where=None)
+
+    def delete(self, subject_type: Tree, subject: Tree, where: Tree):
+        return {
+            "query_type": FSQLQueryType.DELETE,
+            "subject": self.as_value(subject),
+            "subject_type": self.as_fsql_subject_type(subject_type),
+            "where": self.as_where(where)
+        }
+
+    def delete_collection(self, subject_type: Tree, subject: Tree, where: Tree):
+        return self.delete(subject_type, subject, where)
+
+    def delete_document(self, subject_type: Tree, subject: Tree):
+        return self.delete(subject_type, subject, where=None)
